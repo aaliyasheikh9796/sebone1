795bf916f55d12cab7e7d3314927d097bcfe0288
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-17 23:56:08 +0530

    Add files via upload

--- Assignment2/numbers/CountDigit.java
+++ Assignment2/numbers/CountDigit.java
@@ -1,1 +1,43 @@
-# sebone1
+/**class name:- CountDigit
+ * objective :To count the digit in a given number 
+ * 
+ */
+/**
+ * @author Aaliya Sheikh
+ *  date:- 17/03/2022/**
+ * 
+ */
+package com.aaliya.numbers.program;
+import java.util.Scanner;
+/*
+ * methodname-Countdigit
+ * objective:-Taking input from main class 
+ * input parameter:-taking a num (number) as input
+ * */
+public class CountDigit 
+{
+	int count(int num)
+	{
+		int count=0;
+	     if(num==0)
+	        return 1;
+	            
+	    while(num!=0) 
+	    {
+	      num=num/10;
+	      count++;
+	    }
+	 return count;
+	            
+	 }
+ public static void main(String[] args) 
+    {
+	 	Scanner sc=new Scanner(System.in);
+	 	System.out.println("Enter a number:");
+	 	int n=sc.nextInt();
+	 	CountDigit countDigit=new CountDigit();
+	 	//cd.count(n);
+	 	System.out.println("Number of digit is :"+countDigit.count(n));
+	        }
+	        
+}
